8e64cf07ce419b84d3850ab60cf3b2dfab59e873
Michelle Chen <mjiahui.chen@gmail.com>
2024-07-30 23:33:05 -0700

    change train.py

--- model/train.py
+++ model/train.py
@@ -11,10 +11,12 @@ from torch.utils.data import DataLoader, Dataset
 from model import UNET
 import cv2
 
-IMAGE_PATH = os.getenv("IMAGE_PATH", "datafiles/DJI_0002.JPG")
-SEGMENTATION_PATH = os.getenv("SEGMENTATION_PATH", "datafiles/dbh.npy")
-MODEL_SAVE_PATH = os.getenv("MODEL_SAVE_PATH", "datafiles/models/model.pth")
-OUTPUT_IMAGE_PATH = os.getenv("OUTPUT_IMAGE_PATH", "datafiles/generated_outputs/dbh_comparison.png")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+IMAGE_PATH = os.path.join(BASE_DIR, "datafiles/DJI_0002.JPG")
+SEGMENTATION_PATH = os.path.join(BASE_DIR, "datafiles/dbh.npy")
+MODEL_SAVE_PATH = os.path.join(BASE_DIR, "datafiles/models/model.pth")
+OUTPUT_IMAGE_PATH = os.path.join(BASE_DIR, "datafiles/generated_outputs/dbh_comparison.png")
+DBH_FILE = os.path.join(BASE_DIR, "datafiles/dbh.npy")
 
 class CustomDataset(Dataset):
     def __init__(self, image_path, segmentation_path, transform=None):
@@ -65,9 +67,10 @@ def load_data(image_path, segmentation_path):
     transform = transforms.Compose([transforms.ToTensor()])
     image_tensor = transform(image).unsqueeze(0).to(device)
     segmentation_tensor = transform(segmentation).unsqueeze(0).to(device)
-    #normalization
+    #normalization from segmentation to image
     segmentation_tensor = (segmentation_tensor - segmentation_tensor.min()) / (segmentation_tensor.max() - segmentation_tensor.min())
     image_tensor = transform(image).unsqueeze(0).to(device)
+
     return image_tensor, segmentation_tensor
 
 def get_unique_filename(filepath):
@@ -140,15 +143,13 @@ def plot_training_loss(losses):
     plt.show()
 
 if __name__ == "__main__":
-    # dbh_path = "/home/mich889/model/datafiles/dbh.npy"
-    dbh_path = "/Users/michellechen/cs/other/research/nerfforest-model/model/datafiles/dbh.npy"
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     torch.cuda.empty_cache()
 
     # Load the data
-    image_tensor, segmentation_tensor = load_data(image_path, dbh_path)
+    image_tensor, segmentation_tensor = load_data(IMAGE_PATH, DBH_FILE)
 
     print("image tensor shape: ", image_tensor.shape)
     print("segmentation tensor shape: ", segmentation_tensor.shape)
@@ -161,9 +162,7 @@ if __name__ == "__main__":
     trained_model, losses = train(model, image_tensor, segmentation_tensor)
 
     # Save the model
-    model_path_input = "/home/mich889/model/datafiles/model.pth"
-    model_path_input = "/Users/michellechen/cs/other/research/nerfforest-model/model/datafiles/models/model.pth"
-    model_path = get_unique_filename(model_path_input)
+    model_path = get_unique_filename(MODEL_SAVE_PATH)
     torch.save(trained_model.state_dict(), model_path)
     print(f"Model saved at {model_path}")
     
@@ -175,20 +174,24 @@ if __name__ == "__main__":
     print(f"Model output min: {output.min()}, Model output max: {output.max()}")
 
     # Denormalize the model output to match the DBH range
-    dbh_min, dbh_max = np.min(np.load(dbh_path)), np.max(np.load(dbh_path))
+    dbh_min, dbh_max = np.min(np.load(DBH_FILE)), np.max(np.load(DBH_FILE))
     denormalized_output = output * (dbh_max - dbh_min) + dbh_min
-
-    print(f"Model output min: {denormalized_output.min()}, Model output max: {denormalized_output.max()}")
+    print(f"(denormalized) Model output min: {denormalized_output.min()}, (denormalized) Model output max: {denormalized_output.max()}")
 
     # Display the original and predicted DBH side by side
     original_dbh = segmentation_tensor.squeeze().cpu().numpy() * (dbh_max - dbh_min) + dbh_min
     predicted_dbh = denormalized_output.squeeze()
+    pred_min, pred_max = np.min(predicted_dbh), np.max(predicted_dbh)
+    
+    scaled_dbh = ((predicted_dbh - pred_min) * (dbh_max - dbh_min) / (pred_max - pred_min)) + dbh_min
+
 
     # Apply colormap to the images (normalized)
     colormap_original_dbh = cv2.applyColorMap((original_dbh * 255 / dbh_max).astype(np.uint8), cv2.COLORMAP_JET)
     colormap_predicted_dbh = cv2.applyColorMap((predicted_dbh * 255 / dbh_max).astype(np.uint8), cv2.COLORMAP_JET)
+    scaled_colormap_predicted_dbh = cv2.applyColorMap((scaled_dbh * 255 / dbh_max).astype(np.uint8), cv2.COLORMAP_JET)
 
-    fig, axs = plt.subplots(1, 2, figsize=(12, 6))
+    fig, axs = plt.subplots(1, 3, figsize=(18, 6))
     axs[0].imshow(original_dbh, cmap='viridis')
     axs[0].set_title('Original DBH')
     axs[0].axis('off')
@@ -197,10 +200,19 @@ if __name__ == "__main__":
     axs[1].set_title('Predicted DBH')
     axs[1].axis('off')
 
+    axs[2].imshow(scaled_dbh, cmap='viridis')
+    axs[2].set_title('Scaled Predicted DBH')
+    axs[2].axis('off')
+
+    axs[3].imshow(scaled_dbh, cmap='viridis')
+    axs[3].set_title('Loaded model Scaled Predicted DBH')
+    axs[3].axis('off')
+
     print(np.unique(original_dbh))
     print(np.unique(predicted_dbh))
-    print(f"Original DBH min: {original_dbh.min()}, Original DBH max: {original_dbh.max()}")
-    print(f"Predicted DBH min: {predicted_dbh.min()}, Predicted DBH max: {predicted_dbh.max()}")
+    print(np.unique(scaled_dbh))
+    print(f"Original DBH min: {original_dbh.min()}, Original DBH max: {original_dbh.max()}", f"Scaled DBH min: {scaled_dbh.min()}")
+    print(f"Predicted DBH min: {predicted_dbh.min()}, Predicted DBH max: {predicted_dbh.max()}", f"Scaled DBH max: {scaled_dbh.max()}")
 
     # Calculate accuracy with threshold
     threshold = 0.1 
